49935b0e825d9922685ceebf3b7803a537a449d5
k3rnll <kanaikin.greg@gmail.com>
2023-08-13 19:29:37 +0300

    separate "/currency/*" and "/currencies" controllers

---
+++ src/main/java/CurrenciesController.java
@@ -0,0 +1,31 @@
+import org.json.JSONObject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+@WebServlet({"/currencies"})
+public class CurrenciesController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            DBController db = new DBController();
+            Mapper mapper = new Mapper();
+            String output = db.getCurrenciesSet()
+                            .stream()
+                            .map(mapper::toDTO)
+                            .map(JSONObject::new)
+                            .map(JSONObject::toString)
+                            .collect(Collectors.joining(","));
+            out.print(output);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(500);
+        }
+    }
+}
--- src/main/java/CurrencyController.java
+++ src/main/java/CurrencyController.java
@@ -1,4 +1,3 @@
-import org.json.JSONObject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,48 +6,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
-
-@WebServlet({"/currencies", "/currency/*"})
+@WebServlet({"/currency/*"})
 public class CurrencyController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
         String uri = request.getRequestURI();
         DBController db = new DBController();
-        if(uri.endsWith("/currencies")) {
+        String input = uri.substring(uri.indexOf("currency") + 8);
+        String code = parseCurrencyCode(input);
+        if(Objects.isNull(code))
+            response.sendError(400);
+        else {
             try {
-                Mapper mapper = new Mapper();
-                String output =
-                        db.getCurrenciesSet()
-                        .stream()
-                        .map(mapper::toDTO)
-                        .map(JSONObject::new)
-                        .map(JSONObject::toString)
-                        .collect(Collectors.joining(","));
-                out.print(output);
+                Currency currency = db.getCurrency(code);
+                if (Objects.isNull(currency))
+                    response.sendError(404);
+                else
+                    out.print(db.getCurrency(code).toString());
             } catch (SQLException e) {
                 e.printStackTrace();
                 response.sendError(500);
-            }
-        }
-        else {
-            String input = uri.substring(uri.indexOf("currency") + 8);
-            String code = parseCurrencyCode(input);
-            if(Objects.isNull(code))
-                response.sendError(400);
-            else {
-                try {
-                    Currency currency = db.getCurrency(code);
-                    if (Objects.isNull(currency))
-                        response.sendError(404);
-                    else
-                        out.print(db.getCurrency(code).toString());
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    response.sendError(500);
-                }
             }
         }
     }
